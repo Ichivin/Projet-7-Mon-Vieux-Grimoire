1a4031b7e3562e343ddca0aa199b10ee79704ef7
Ichivin <kevinblondiau@gmail.com>
2024-02-08 01:05:12 +0100

    add routes and controllers

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const Book = require("./models/Book");
+const bookRoutes = require("./routes/book");
 
 const app = express();
 
@@ -18,36 +18,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post("/api/stuff", (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body,
-    });
-    book.save()
-        .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-        .catch((error) => res.status(400).json({ error }));
-});
-
-app.use("/api/stuff", (req, res, next) => {
-    const stuff = [
-        {
-            _id: "oeihfzeoi",
-            title: "Mon premier objet",
-            description: "Les infos de mon premier objet",
-            imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 4900,
-            userId: "qsomihvqios",
-        },
-        {
-            _id: "oeihfzeomoihi",
-            title: "Mon deuxième objet",
-            description: "Les infos de mon deuxième objet",
-            imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-            price: 2900,
-            userId: "qsomihvqios",
-        },
-    ];
-    res.status(200).json(stuff);
-});
+app.use("/api/book", bookRoutes);
 
 module.exports = app;
